12dfe45adff291130f88752c8fae2eb11e62bce4
arash3f <arash.alfooneh@gmail.com>
2024-04-04 00:13:53 +0330

    :hammer: ref(global): decorators
    refactor all base decorators
    
    ISSUES CLOSED: 5

--- src/common/decorators/get-ip.decorator.ts
+++ src/common/decorators/get-ip.decorator.ts
@@ -1,9 +1,8 @@
 import type { ExecutionContext } from "@nestjs/common"
 import { createParamDecorator } from "@nestjs/common"
 import type { FastifyRequest } from "fastify"
-
 /**
- * Decorator to get the requester's IP
+ * * Decorator to get the requester's IP
  */
 export const GetIp = createParamDecorator((_data: unknown, context: ExecutionContext) => {
 	const request = context.switchToHttp().getRequest<FastifyRequest>()
--- src/common/decorators/get-user-id.decorator.ts
+++ src/common/decorators/get-user-id.decorator.ts
@@ -1,12 +1,13 @@
 import type { ExecutionContext } from "@nestjs/common"
 import { createParamDecorator } from "@nestjs/common"
 import type { FastifyRequest } from "fastify"
+
 import type { TokenGuardData } from "../types/token.type"
 
 /**
- * Decorator to get the requester's UserId
+ * * Decorator to get the requester's UserId
  *
- * Note: this decorator does not authenticate requester
+ * ! Note: this decorator does not authenticate requester
  */
 export const GetUserId = createParamDecorator((_data: unknown, context: ExecutionContext) => {
 	const request = context.switchToHttp().getRequest<FastifyRequest>()
