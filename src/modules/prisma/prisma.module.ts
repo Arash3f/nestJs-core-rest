fa52ca3db68a9658429ffdac7114ebdca0212186
arash3f <arash.alfooneh@gmail.com>
2024-04-04 04:09:56 +0330

    :hammer: ref(global): prisma module
    refactor prisma module
    
    ISSUES CLOSED: 13

--- src/modules/prisma/prisma.module.ts
+++ src/modules/prisma/prisma.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common"
 import { PrismaService } from "src/modules/prisma/prisma.service"
 
+import { EnvConfigModule } from "../config/env-config.module"
+
 @Module({
   exports: [
     PrismaService,
+  ],
+  imports: [
+    EnvConfigModule,
   ],
   providers: [
     PrismaService,
--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,47 +1,51 @@
 import type { OnModuleDestroy, OnModuleInit } from "@nestjs/common"
 import { Injectable, Logger } from "@nestjs/common"
-import { PrismaClient } from "@prisma/client"
+import { Prisma, PrismaClient } from "@prisma/client"
+import { EnvConfigService } from "src/modules/config/env-config.service"
+import { NodeEnvType } from "src/modules/config/types/config.type"
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
 	private readonly logger = new Logger(PrismaService.name)
 
-	constructor() {
-		super()
-		// TODO: This should be configurable
-		// super({
-		// 	log: [
-		// 		{
-		// 			emit: "event",
-		// 			level: "query"
-		// 		},
-		// 		{
-		// 			emit: "stdout",
-		// 			level: "error"
-		// 		},
-		// 		{
-		// 			emit: "stdout",
-		// 			level: "info"
-		// 		},
-		// 		{
-		// 			emit: "stdout",
-		// 			level: "warn"
-		// 		}
-		// 	]
-		// })
+	constructor(
+		private readonly envConfigService: EnvConfigService,
+	) {
+		super({
+			
+			log: [
+				{
+					emit: "event",
+					level: "query",
+				},
+				{
+					emit: "stdout",
+					level: "error",
+				},
+				{
+					emit: "stdout",
+					level: "info",
+				},
+				{
+					emit: "stdout",
+					level: "warn",
+				},
+			],
+		})
 	}
 
 	async onModuleInit() {
 		await this.$connect()
-		// if (process.env.NODE_ENV === NodeEnvType.Development) {
-		//   this.$on("query", (e) => {
-		//     this.logger.verbose({
-		//       Query: e.query,
-		//       Params: e.params,
-		//       Duration: String(e.duration) + "ms"
-		//     })
-		//   })
-		// }
+		if (this.envConfigService.nodeEnv === NodeEnvType.Development) {
+			this.$on("query" as never, (e) => {
+				const res: Prisma.QueryEvent = e
+				this.logger.verbose({
+					Query: res.query,
+					Params: res.params,
+					Duration: String(res.duration) + "ms",
+				})
+		  })
+		}
 	}
 
 	async onModuleDestroy() {
