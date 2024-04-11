277c9c9e30ec778a8291d3633f1513500a1411fd
arash3f <arash.alfooneh@gmail.com>
2024-04-11 14:46:16 +0330

    :hammer: ref(global):

--- src/modules/auth/auth.e2e-spec.ts
+++ src/modules/auth/auth.e2e-spec.ts
@@ -351,6 +351,7 @@ describe("Auth", () => {
      */
 
     it("+ ReadUsers API Successfuly (Where)", async () => {
+        expect(1).toBe(2)
         /**
          * * Create some user
          */
