2754b7aa5b412e6d63ee0a49b88a0b5ad2e45f77
arash alfooneh <arash.alfooneh@gmail.com>
2026-06-05 16:40:11 +0330

    :sparkles: feat(global): swagger errors for api

---
+++ src/common/dto/error-response.dto.ts
@@ -0,0 +1,48 @@
+// dto/error-response.dto.ts
+import { ApiProperty } from "@nestjs/swagger"
+import { ModuleNames } from "@src/constants"
+
+export class AppExceptionResponseDto {
+  @ApiProperty({
+    description: "HTTP status code",
+  })
+  statusCode: number
+
+  @ApiProperty({
+    description: "Error message in English",
+  })
+  message: string
+
+  @ApiProperty({
+    description: "Error message in Persian",
+  })
+  persianTranslation: string
+
+  @ApiProperty({
+    description: "Detailed error message for developers",
+    required: false,
+  })
+  developerMessage?: string
+
+  @ApiProperty({
+    description: "Module special error code",
+  })
+  code: number
+
+  @ApiProperty({
+    description: "Module where error occurred",
+    enum: ModuleNames,
+  })
+  module: ModuleNames
+
+  @ApiProperty({
+    description: "Timestamp of the error",
+    example: "2024-01-01T10:00:00.000Z",
+  })
+  timestamp: string
+
+  @ApiProperty({
+    description: "Request path",
+  })
+  path?: string
+}
